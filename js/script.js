33b7ac80365db0982cec8b5031f5d4f0eb582f7a
LucianoR8 <luciribsantos@gmail.com>
2025-06-09 21:29:25 -0300

    Update 0.2

--- js/script.js
+++ js/script.js
@@ -18,25 +18,33 @@ document.addEventListener("DOMContentLoaded", function (){
 
         const imc = peso / (altura * altura);
         let grau = "";
+        let obs = "";
 
         if (imc < 18.5){
             grau = "Abaixo do peso";
+            obs = "Algumas pessoas possuem um baixo peso por características do seu organismo. Outras podem estar enfrentando problemas, como a desnutrição. Procure um médico para saber qual é o caso.";
         } else if (imc < 24.9){
             grau = "Peso ideal";
+            obs = "O melhor jeito de continuar assim é mantendo um estilo de vida ativo e uma alimentação saudável.";
         } else if (imc < 29.9){
             grau = "Sobrepeso";
+            obs = "Pré-obesidade muitas pessoas nessa faixa de peso já apresentam doenças associadas, como diabetes e hipertensão. Reveja seus hábitos e busque ajuda antes de entrar na faixa da obesidade.";
         } else if (imc < 34.9){
             grau = "Obesidade I";
+            obs = "Alerta! Chegou na hora de se cuidar. Vamos dar início a mudanças hoje! Cuide de sua alimentação e se exercite. Você precisa iniciar um acompanhamento com nutricionista e/ou endocrinologista.";
         } else if ( imc < 39.9){
             grau = "Obesidade II";
+            obs = "Inicie mudanças no estilo de vida com o acompanhamento próximo de profissionais de saúde.";
         } else {
             grau = "Obesidade III";
+            obs = "Sinal vermelho, com forte probabilidade de já existirem doenças muito graves associadas. busque por um tratamento urgentemente.";
         }
 
         const resultado = document.querySelector(".container-imc");
         resultado.innerHTML = `
         <p>Seu IMC é: ${imc.toFixed(2)}</p>
         <p>Grau: ${grau}</p>
+        <p>Observação: ${obs}</p>
         `;
     });
 
